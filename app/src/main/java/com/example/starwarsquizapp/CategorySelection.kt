ffe49cd38eb4f12d2a8531d8b0162598b3c1265f
Charles Bird <BIR23001223@students.wnc.ac.uk>
2024-04-18 12:03:50 +0100

    add new activity

---
+++ app/src/main/java/com/example/starwarsquizapp/CategorySelection.kt
@@ -0,0 +1,11 @@
+package com.example.starwarsquizapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CategorySelection : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.category_selection)
+    }
+}
